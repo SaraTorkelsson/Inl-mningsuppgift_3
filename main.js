06de89bafbad65924e208fb983981559810ce0d3
SaraTorkelsson <89971904+SaraTorkelsson@users.noreply.github.com>
2021-11-19 14:12:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// 
+function changeMode() {
+    var element = document.body;
+    element.classList.toggle("darkMode");
+}
+
+// document.body.style.backgroundColor = "#522F85";
+//     document.body.style.color = "White";
+
+// <h3>Batman är starkast i DC-Universe!</h3>
+// <input type="radio" value="true" name="sant" />
+// <label for="sant">Sant</label>
+
+// changeMode.addEventListener("click", () => {
+//     let body = document.querySelector("body");
+//     if (body.style.background === "white") {
+//     body.style.background = "white";
+//     } 
+//     else {
+//     body.style.background = "yellow";
+//     }
+//     });
