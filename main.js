1cd78cf19d39d335d5f88ca4f614e361d9e85d1d
SaraTorkelsson <89971904+SaraTorkelsson@users.noreply.github.com>
2021-11-30 20:21:53 +0100

    Add files via upload
    Ändrade i JS

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
-// 
+// Funktion som ändrar till darkmode
 function changeMode() {
     var element = document.body;
     element.classList.toggle("darkMode");
 }
 
-// document.body.style.backgroundColor = "#522F85";
-//     document.body.style.color = "White";
+let getResults = document.querySelector("#getResults");
+
+// Skriver ut sista resultatet och visar passande färg.
+// let finalScore = document.createElement
+
+
+// getResults.addEventlistener("click", () => {
+//     let counter = 0;
+//     if (document.getElementById(""))
+// })
+
+
+
+
+if (result == 7) {
+    "Grattis! Du hade alla rätt!"
+    finalScore.style.color = green;
+} 
+else if (result > 3){
+    "Snyggt jobbat! Du hade mer än hälften rätt!"
+    finalScore.style.color = orange;
+}
+else {
+    "Försök igen! Du hade mindre än hälften rätt!"
+    finalScore.style.color = red;
+}
 
 // <h3>Batman är starkast i DC-Universe!</h3>
 // <input type="radio" value="true" name="sant" />
 // <label for="sant">Sant</label>
 
-// changeMode.addEventListener("click", () => {
-//     let body = document.querySelector("body");
-//     if (body.style.background === "white") {
-//     body.style.background = "white";
-//     } 
-//     else {
-//     body.style.background = "yellow";
+// let quizAnswer = () => {
+//   inputs.forEach((input) => {
+//     if(input.checked){
+//       answer = input.value;
+//     if(answer == "true"){
+//         count++;
+//       }
 //     }
-//     });
+//   });
